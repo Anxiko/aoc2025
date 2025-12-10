487407e3cb620278b8ca45e1af84f49082c93a2e
Joaquín Monteagudo Gómez <Kindos7@gmail.com>
2025-12-10 23:16:14 +0100

    Part 2 working for example, really slow

--- lib/aoc2025/solutions/day10.ex
+++ lib/aoc2025/solutions/day10.ex
@@ -1,16 +1,25 @@
 defmodule Aoc2025.Solutions.Day10 do
-  @enforce_keys [:target_lights, :current_lights, :buttons, :joltage_requirements]
+  @enforce_keys [
+    :target_lights,
+    :current_lights,
+    :buttons,
+    :target_joltages,
+    :current_joltages
+  ]
   defstruct @enforce_keys
 
   @type lights() :: [boolean()]
   @type button() :: [non_neg_integer()]
   @type joltage() :: pos_integer()
+  @type target() :: :lights | :joltages
+  @type status() :: :complete | :cont | :halt
 
   @type t() :: %__MODULE__{
           target_lights: lights(),
           current_lights: lights(),
           buttons: [button()],
-          joltage_requirements: [joltage()]
+          target_joltages: [joltage()],
+          current_joltages: [joltage()]
         }
 
   @machine_pattern ~r"^\[(?P<lights>[.#]+)\] (?P<buttons>((\(\d+(,\d+)*)\) )+)\{(?P<joltages>\d+(,\d+)*)\}$"
@@ -21,21 +30,25 @@ defmodule Aoc2025.Solutions.Day10 do
   def solve(input, part, _mode) do
     machines = Enum.map(input, &parse/1)
 
-    case part do
-      :part1 ->
-        machines
-        |> Enum.map(&min_presses/1)
-        |> Enum.sum()
-    end
+    target =
+      case part do
+        :part1 -> :lights
+        :part2 -> :joltages
+      end
+
+    machines
+    |> Enum.map(&min_presses(&1, target))
+    |> Enum.sum()
   end
 
   @spec new(lights(), [button()], [joltage()]) :: t()
-  def new(target_lights, buttons, joltage_requirements) do
+  def new(target_lights, buttons, target_joltages) do
     %__MODULE__{
       target_lights: target_lights,
       current_lights: List.duplicate(false, length(target_lights)),
       buttons: buttons,
-      joltage_requirements: joltage_requirements
+      target_joltages: target_joltages,
+      current_joltages: List.duplicate(0, length(target_joltages))
     }
   end
 
@@ -44,18 +57,18 @@ defmodule Aoc2025.Solutions.Day10 do
     %{"lights" => lights, "buttons" => buttons, "joltages" => joltages} =
       Regex.named_captures(@machine_pattern, machine)
 
-    new(parse_lights(lights), parse_buttons(buttons), parse_joltage_requirements(joltages))
+    new(parse_lights(lights), parse_buttons(buttons), parse_target_joltages(joltages))
   end
 
-  @spec min_presses(t()) :: non_neg_integer()
-  def min_presses(%__MODULE__{} = machine) do
-    min_presses([machine], MapSet.new(), 0)
+  @spec min_presses(t(), target()) :: non_neg_integer()
+  def min_presses(%__MODULE__{} = machine, target) do
+    min_presses([machine], target, MapSet.new(), 0)
   end
 
-  @dialyzer {:nowarn_function, min_presses: 3}
-  @spec min_presses([t()], MapSet.t(t()), non_neg_integer()) :: non_neg_integer()
-  defp min_presses(machines, %MapSet{} = seen, presses) do
-    if Enum.any?(machines, &solved?/1) do
+  @dialyzer {:nowarn_function, min_presses: 4}
+  @spec min_presses([t()], target(), MapSet.t(t()), non_neg_integer()) :: non_neg_integer()
+  defp min_presses([_|_] = machines, target, %MapSet{} = seen, presses) do
+    if Enum.any?(machines, &(completion_status(&1, target) == :complete)) do
       presses
     else
       machines =
@@ -64,8 +77,9 @@ defmodule Aoc2025.Solutions.Day10 do
           Enum.map(machine.buttons, &press_button(machine, &1))
         end)
         |> Enum.reject(&MapSet.member?(seen, &1))
+        |> Enum.reject(&(completion_status(&1, target) == :halt))
 
-      min_presses(machines, machines |> MapSet.new() |> MapSet.union(seen), presses + 1)
+      min_presses(machines, target, machines |> MapSet.new() |> MapSet.union(seen), presses + 1)
     end
   end
 
@@ -82,7 +96,37 @@ defmodule Aoc2025.Solutions.Day10 do
         end
       end)
 
-    %{machine | current_lights: lights}
+    joltages =
+      machine.current_joltages
+      |> Enum.with_index()
+      |> Enum.map(fn {value, index} ->
+        if index in button do
+          value + 1
+        else
+          value
+        end
+      end)
+
+    %{machine | current_lights: lights, current_joltages: joltages}
+  end
+
+  @spec completion_status(t(), target()) :: status()
+  def completion_status(%__MODULE__{} = machine, :lights) do
+    if machine.current_lights == machine.target_lights do
+      :complete
+    else
+      :continue
+    end
+  end
+
+  def completion_status(%__MODULE__{} = machine, :joltages) do
+    machine.current_joltages
+    |> Enum.zip(machine.target_joltages)
+    |> Enum.reduce_while(:complete, fn
+      {current, target}, _ when current > target -> {:halt, :halt}
+      {target, target}, :complete -> {:cont, :complete}
+      _, _ -> {:cont, :cont}
+    end)
   end
 
   @spec solved?(t()) :: boolean()
@@ -108,8 +152,8 @@ defmodule Aoc2025.Solutions.Day10 do
     end)
   end
 
-  @spec parse_joltage_requirements(String.t()) :: [joltage()]
-  defp parse_joltage_requirements(joltages) do
+  @spec parse_target_joltages(String.t()) :: [joltage()]
+  defp parse_target_joltages(joltages) do
     joltages
     |> String.split(",")
     |> Enum.map(&String.to_integer/1)
